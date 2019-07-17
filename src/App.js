0ec7c0af8d93097a6e3416b8b421c5b6b8bebdff
damienteo <ksteo89@gmail.com>
2019-07-17 23:29:36 +0800

    Add basic functionality

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React, { Component } from "react";
 
 class App extends Component {
   state = { language: "english" };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a Language:{" "}
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("dutch")}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    );
   }
 }
 
